6b120f4891a3cc87b5a34eaabd75924b35298b20
ThinkerJack <54969139+ThinkerJack@users.noreply.github.com>
2022-01-20 19:36:43 +0800

    change

--- flutter_demo/lib/main.dart
+++ flutter_demo/lib/main.dart
@@ -21,7 +21,73 @@ class MyApp extends StatelessWidget {
       // locale: DevicePreview.locale(context), // Add the locale here
       // builder: DevicePreview.appBuilder,
       title: 'Flutter Demo',
-      home: Index(),
+      home: Test(),
     );
+  }
+}
+class Test extends StatefulWidget {
+  const Test({Key? key}) : super(key: key);
+
+  @override
+  _TestState createState() => _TestState();
+}
+
+class _TestState extends State<Test> {
+  List<double> list = [];
+  int count = 0;
+
+  @override
+  void didUpdateWidget(Test oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    print("didUpdateWidget me");
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: TestView(list, count),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            list.add(0);
+            count++;
+          });
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+class TestView extends StatefulWidget {
+  final List<double> list;
+  final int count;
+
+  TestView(this.list, this.count);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _TestViewState();
+  }
+}
+
+class _TestViewState extends State<TestView> {
+  @override
+  void initState() {
+    super.initState();
+    print("initState");
+  }
+
+  @override
+  void didUpdateWidget(TestView oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    print("didUpdateWidget");
+    print("list:${oldWidget.list.length}:${widget.list.length}");
+    print("count:${oldWidget.count}:${widget.count}");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print("build");
+    return Container();
   }
 }
